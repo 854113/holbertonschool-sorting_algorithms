cb91f1db4bed84b721d867251eae863e2ad0f308
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-08-10 16:30:00 -0300

    Create 1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sort a doubly linked list of integers in ascending order
+ * using the Insertion sort algorithm.
+ * @list: Double pointer to the head of the list.
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+  listint_t *curr, *prev;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	curr = (*list)->next;
+	while (curr)
+	{
+		prev = curr->prev;
+		while (prev && prev->n > curr->n)
+		{
+			
+			if (prev->prev)
+				prev->prev->next = curr;
+			else
+				*list = curr;
+
+			if (curr->next)
+				curr->next->prev = prev;
+
+			prev->next = curr->next;
+			curr->prev = prev->prev;
+			curr->next = prev;
+			prev->prev = curr;
+
+			print_list(*list);
+
+			prev = curr->prev;
+		}
+		curr = curr->next;
+	}
+}
