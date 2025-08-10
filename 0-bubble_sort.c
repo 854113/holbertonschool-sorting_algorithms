2c2cf92ccc1e88fab122c17b255c8bfb70118299
Bruno Salle <171603369+854113@users.noreply.github.com>
2025-08-10 16:16:30 -0300

    Create 0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t route, count;
+	int tmp;
+
+	for (route = 0; route < size - 1; route++)
+	{
+    for (count = 0; count < size - 1 - route; count++)
+		{
+			if (array[count] > array[count + 1])
+			{
+				tmp = array[count];
+				array[count] = array[count + 1];
+				array[count + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
